6c436cd79d18767b9db001b55a786ee5b9c4af91
Thaetami <tatanghaetami.97@gmail.com>
2023-10-02 18:35:36 +0700

    update shipment page

--- src/app/pages/shipment-page/shipment-page.component.ts
+++ src/app/pages/shipment-page/shipment-page.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./shipment-page.component.css']
 })
 export class ShipmentPageComponent {
+  isCollapse: boolean = true;
+
+  toggleCollapse() {
+    this.isCollapse = !this.isCollapse;
+  }
 
 }
